a20935e7283c79371264985860d1daa819bfd443
xiaowansm <31873594+xiaowansm5@users.noreply.github.com>
2021-08-02 23:42:35 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -3,19 +3,23 @@
 # AutoBuild DiyScript
 
 Diy_Core() {
-	Author=小碗儿数码
-	Short_Firmware_Date=true
+	Author=小碗儿说明
 	Default_LAN_IP=192.168.5.1
+	Load_CustomPackages_List=true
+	Short_Firmware_Date=true
+	Checkout_Virtual_Images=false
 
 	INCLUDE_AutoBuild_Features=true
-	INCLUDE_DRM_I915=false
+	INCLUDE_DRM_I915=true
 	INCLUDE_Argon=true
 	INCLUDE_Obsolete_PKG_Compatible=false
-	
-	Load_CustomPackages_List=true
-	Checkout_Virtual_Images=false
 }
 
 Firmware-Diy() {
-	:
+	case "${TARGET_PROFILE}" in
+	d-team_newifi-d2)
+		Copy CustomFiles/mac80211.sh package/kernel/mac80211/files/lib/wifi
+		Copy CustomFiles/system_${TARGET_PROFILE} package/base-files/files/etc/config system
+	;;
+	esac
 }
